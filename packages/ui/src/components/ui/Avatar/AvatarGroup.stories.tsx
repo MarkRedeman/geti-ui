5f1e23cb0935394b0dadf096997b97476e016ffa
Mark Redeman <markredeman@gmail.com>
2026-03-08 08:13:55 +0100

    feat(avatar): add dedicated AvatarGroup stories

---
+++ packages/ui/src/components/ui/Avatar/AvatarGroup.stories.tsx
@@ -0,0 +1,41 @@
+// Copyright (C) 2022-2025 Intel Corporation
+// LIMITED EDGE SOFTWARE DISTRIBUTION LICENSE
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { AvatarGroup } from '@geti/ui';
+
+const meta: Meta<typeof AvatarGroup> = {
+    tags: ['!dev'],
+    component: AvatarGroup,
+    title: 'UI/AvatarGroup',
+    parameters: {
+        a11y: {},
+    },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof AvatarGroup>;
+
+const avatars = [
+    { src: 'https://i.pravatar.cc/150?img=11', alt: 'User 1' },
+    { src: 'https://i.pravatar.cc/150?img=12', alt: 'User 2' },
+    { src: 'https://i.pravatar.cc/150?img=13', alt: 'User 3' },
+    { src: 'https://i.pravatar.cc/150?img=14', alt: 'User 4' },
+    { src: 'https://i.pravatar.cc/150?img=15', alt: 'User 5' },
+];
+
+export const Default: Story = {
+    args: {
+        avatars,
+        max: 3,
+    },
+};
+
+export const NoOverflow: Story = {
+    args: {
+        avatars: avatars.slice(0, 3),
+        max: 3,
+    },
+};
