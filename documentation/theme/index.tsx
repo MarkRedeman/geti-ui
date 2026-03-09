c3bc6c9442264abbf508dc7b77e73952a30d6ee5
Mark Redeman <markredeman@gmail.com>
2026-03-09 13:53:13 +0100

    docs(documentation): polish theme and split homepage control categories

--- documentation/theme/index.tsx
+++ documentation/theme/index.tsx
@@ -1,6 +1,7 @@
 import { Layout as OriginalLayout } from '@rspress/core/theme-original';
 import { ThemeProvider } from '@geti/ui';
 import '@geti/ui/styles.css';
+import './index.css';
 
 export const Layout = () => {
   return (
