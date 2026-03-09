f71f9e036bc32339dfa9d8f76e6eb4bc4f6059b5
Mark Redeman <markredeman@gmail.com>
2026-03-09 10:16:28 +0100

    feat(smart-tools): port opencv tools and model wrappers

---
+++ packages/smart-tools/src/watershed/interfaces.ts
@@ -0,0 +1,16 @@
+// Copyright (C) 2022-2025 Intel Corporation
+// LIMITED EDGE SOFTWARE DISTRIBUTION LICENSE
+
+import { Point } from '../shared/interfaces';
+
+export interface WatershedPolygon {
+    id: number;
+    label: { id: string };
+    points: Point[];
+}
+
+export type Marker = {
+    id: number;
+    label: { id: string };
+    points: Point[];
+};
